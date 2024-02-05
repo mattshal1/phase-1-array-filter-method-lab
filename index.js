25225cf641677b774dafc94b8e93c44ce99c4ba5
Ahmed Shalash <ah.shalash@gmail.com>
2024-02-06 00:10:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+const drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+  ];
+  
+  // Function to find matching drivers by name
+  function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Function to find drivers whose names begin with provided letters
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // Function to find drivers whose name property matches provided string
+  function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+  }
+  
+  // Test Cases
+  console.log(findMatching(drivers, 'Bobby')); // [{ name: 'Bobby', hometown: 'Pittsburgh' }, { name: 'Bobby', hometown: 'Tampa Bay' }]
+  console.log(fuzzyMatch(drivers, 'sa')); // [{ name: 'Sammy', hometown: 'New York' }, { name: 'Sally', hometown: 'Cleveland' }]
+  console.log(matchName(drivers, 'Bobby')); // [{ name: 'Bobby', hometown: 'Pittsburgh' }, { name: 'Bobby', hometown: 'Tampa Bay' }]
+  
